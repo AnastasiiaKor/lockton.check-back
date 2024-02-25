f2627223c3fd3490a0bdb6eb95e2eaead4c9e5f7
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2024-02-25 13:51:53 +0500

    adds logs

--- controllers/api.js
+++ controllers/api.js
@@ -6,15 +6,16 @@ const getInfo = async (req, res) => {
     const apiKeyName = process.env.API_KEY;
     const apiKeyValue = process.env.KEY_VALUE;
     const apiUrl = `https://my3.soliq.uz/api/remote-access-api/company/info/${id}?type=full`;
-
+    console.log(apiKeyValue, apiKeyName);
     const response = await axios.get(apiUrl, {
       headers: {
         [apiKeyName]: apiKeyValue,
       },
     });
-
+    console.log(response);
     res.status(200).json(response.data);
   } catch (error) {
+    console.log(error);
     if (error.response && error.response.status) {
       if (error.response.data) {
         const responseBody = error.response.data;
